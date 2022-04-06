66d323e40ad20aa0df5b5fc64d9a3c6688796a2c
Florian de Boissieu <fdeboiss@gmail.com>
2022-04-06 15:26:39 +0200

    remove filterMinPoints in predict.sh

--- predict.sh
+++ predict.sh
@@ -20,6 +20,7 @@ if [ "$1" ]; then
   resultfile=$dir/${root}_result.asc
   $scriptsroot/vote/aggregate $predfile $counterfile $resultfile
   echo "* vote : OK"
+  rm $filterMinPoints
   rm $pcafile
   rm $chunkedfile
   rm $counterfile
