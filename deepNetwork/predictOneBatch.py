156366d31f3316133adc6c5d9eef620ea6ac8ee3
Jules Morel <jules.morel@ifpindia.org>
2022-08-17 17:31:10 +0200

    fixed class to fit c++ interface

--- deepNetwork/predictOneBatch.py
+++ deepNetwork/predictOneBatch.py
@@ -12,37 +12,21 @@ import utils as utils
 
 from pointnet2_msg_sem import Pointnet2MSG as Pointnet
 
-def predict(points,model):
-
-    batch_size=32
-
-    use_pca=True
-    use_intensity=False
-    # Size of the batch
-    num_points = len(points)
-    # number of categories
-    k = 2
-
-    data_pred_center = utils.recenterPoints(points,num_points,use_pca,use_intensity)
-    if use_intensity:
-        data_pred_center = data_pred_center.reshape(-1, num_points, 7)
-    else:
+class inference:
+    def __init__(self,model=None):
+        self.model = Pointnet(num_classes=2, input_channels=3, use_xyz=True)
+        self.model.cuda()
+        self.model = torch.load(model)
+        self.model.eval()
+        
+    def run(self,points):  
+        # Size of the batch
+        num_points = len(points)
+        data_pred_center = utils.recenterPoints(points,num_points,True,False)
         data_pred_center = data_pred_center.reshape(-1, num_points, 6)
-
-    if use_intensity:
-        model = Pointnet(num_classes=2, input_channels=4, use_xyz=True)
-    else:
-        model = Pointnet(num_classes=2, input_channels=3, use_xyz=True)
-    model.cuda()
-    model = torch.load(model)
-    model.eval()
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    pred = model(torch.from_numpy(data_pred_center).float().to(device))
-    prednp = pred.cpu().detach().numpy()
-
-    arr_id=np.zeros(num_points);
-    np.where(prednp[0,:,0]<0, arr_id, 1)
-
-    return arr_id
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        pred = self.model(torch.from_numpy(data_pred_center).float().to(device))
+        prednp = pred.cpu().detach().numpy()
+        arr_p0=prednp[0,:,0]
+        arr_p1=prednp[0,:,1]
+        return np.concatenate([arr_p0, arr_p1])
